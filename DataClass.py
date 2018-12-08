91f5a376f2d9747fbcb0dc875e9e7ba177a1cfcd
osnatairy <osnat.drain@biu.ac.il>
2018-12-08 23:42:13 +0200

    Add files via upload

--- DataClass.py
+++ DataClass.py
@@ -1,10 +1,11 @@
 import sys
-import numpy as np
+from collections import defaultdict
 
 class Data:
     def __init__(self, fileName):
         self.sentences = []
-        self.num_of_words={}
+        self.linesInFile = []
+        self.num_of_words = defaultdict(int)
         self.file_name = fileName
         # tags of content words based on penn tree bank tagset, we mostly chose the tags of nouns, verbs and adjectives
         self.content_words_tags = set(['JJ', 'JJR', 'JJS', 'NN', 'NNS', 'NNP', 'NNPS', 'RB', 'RBR', 'RBS', 'VB',
@@ -18,19 +19,24 @@ class Data:
 
     def readData(self):
         with open(file_name, 'r', encoding="utf8") as f:
-            self.sentences = f.readlines()
+            self.linesInFile = f.readlines()
         print("finished reading data")
     def countNumOfWords(self):
-        for sentence in self.sentences:
-            splitted_sentence = sentence.split('\t')
-            if len(splitted_sentence) != 10:
+        sentence = []
+        start = True
+        for line in self.linesInFile:
+            line = line.rstrip().split('\t')
+            if line[0] == '1':
+                if start:
+                    start = False
+                    continue
+                self.sentences.append(sentence)
+                sentence = []
+            sentence.append(line)
+            if len(line) != 10:
                 continue
-            splitted_sentence[-1] = splitted_sentence[-1].replace('\n', '')
-            for word in splitted_sentence:
-                if word in self.num_of_words:
-                    self.num_of_words[word] += 1
-                else:
-                    self.num_of_words[word] = 1
+            self.num_of_words[line[2]] +=1
+
     def filterWords(self):
         for key in self.num_of_words:
             if self.num_of_words[key] < self.threshold:
@@ -50,8 +56,3 @@ if __name__ == '__main__':
     if len(sys.argv) > 0:
         file_name = sys.argv[1]
     data_object = Data(file_name)
-
-
-
-
-
